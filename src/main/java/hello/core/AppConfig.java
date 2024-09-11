f2e697090220b5f5b9dec177cbe7107559e707a6
quddkflr <nmtoy342@naver.com>
2024-09-11 21:50:24 +0900

    study8 commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //call Appconfig.memberService
+    //call Appconfig.memberRepository
+    //call Appconfig.memberRepository
+    //call Appconfig.orderService
+    //call Appconfig.memberRepository
+
+    //call Appconfig.memberService
+    //call Appconfig.memberRepository
+    //call Appconfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
